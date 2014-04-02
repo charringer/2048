dfa1d97a9c2c71fa7bc69af12ef0d8bbd62e881e
Claudio Harringer <der.claudio@gmail.com>
2014-04-02 02:02:41 +0200

    fix minor mistake

--- js/game_manager.js
+++ js/game_manager.js
@@ -72,7 +72,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0; // zero-mod here :)
+    var value = 0; // zero-mod here :)
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
